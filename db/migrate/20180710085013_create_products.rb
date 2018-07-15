ffd9325c654c0af915406611b04d8b368210e201
voblav <voblav.aukro@gmail.com>
2018-07-15 19:31:52 +0300

    add first book

--- db/migrate/20180710085013_create_products.rb
+++ db/migrate/20180710085013_create_products.rb
@@ -2,6 +2,7 @@ class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
       t.string :title, null: false
+      t.text :author, null: false
       t.text :description
       t.decimal :price, precision: 15, scale: 2
 
